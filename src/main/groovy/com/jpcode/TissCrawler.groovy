a2e715c5e70d2a2f0376fdec5087820597ef6ae7
João Pedro Vaz <joaopvgarbin@gmail.com>
2026-09-24 16:18:57 -0300

    feat: Iniciando classe TissCrawler

--- src/main/groovy/com/jpcode/TissCrawler.groovy
+++ src/main/groovy/com/jpcode/TissCrawler.groovy
@@ -1,4 +1,14 @@
 package com.jpcode
 
+import groovyx.net.http.HttpBuilder
+
 class TissCrawler {
+    void acessar() {
+       
+        def response = HttpBuilder.configure {
+            request.uri = "https://www.gov.br/ans/pt-br"
+        }.get()
+
+        println(response)
+    }
 }
