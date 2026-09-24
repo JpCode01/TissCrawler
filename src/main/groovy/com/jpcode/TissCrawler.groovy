8fda23357f68694d36ecad6699419ee6b432ddf3
João Pedro Vaz <joaopvgarbin@gmail.com>
2026-09-24 16:45:04 -0300

    feat: Acessando a pagina do prestador

--- src/main/groovy/com/jpcode/TissCrawler.groovy
+++ src/main/groovy/com/jpcode/TissCrawler.groovy
@@ -1,14 +1,32 @@
 package com.jpcode
 
 import groovyx.net.http.HttpBuilder
+import org.jsoup.nodes.Document
+import org.jsoup.nodes.Element
 
 class TissCrawler {
     void acessar() {
-       
-        def response = HttpBuilder.configure {
+
+        Document document = HttpBuilder.configure {
             request.uri = "https://www.gov.br/ans/pt-br"
         }.get()
 
-        println(response)
+        Element linkEncontrado = document.select('a').find() { link ->
+            link.text().contains("Espaço do Prestador de Serviços de Saúde")
+        }
+
+        if(linkEncontrado) {
+            String urlPrestador = linkEncontrado.absUrl('href')
+
+            Document documentPrestador = HttpBuilder.configure {
+                request.uri = urlPrestador
+            }.get()
+
+            println("Página do prestador acessado!")
+            println(document.title())
+        } else {
+            println("Link não encontrado")
+        }
+
     }
 }
