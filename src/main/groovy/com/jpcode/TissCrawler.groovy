33162f620c92df4ec3be584f34dd0cc56f096ef7
João Pedro Vaz <joaopvgarbin@gmail.com>
2026-09-24 18:36:38 -0300

    feat: Criando metodo para baixar o componente

--- src/main/groovy/com/jpcode/TissCrawler.groovy
+++ src/main/groovy/com/jpcode/TissCrawler.groovy
@@ -5,28 +5,85 @@ import org.jsoup.nodes.Document
 import org.jsoup.nodes.Element
 
 class TissCrawler {
-    void acessar() {
+
+    private final TissParser parser = new TissParser()
+
+    void baixarComponente() {
+
+        println System.getProperty("user.dir")
 
         Document document = HttpBuilder.configure {
             request.uri = "https://www.gov.br/ans/pt-br"
         }.get()
 
-        Element linkEncontrado = document.select('a').find() { link ->
-            link.text().contains("Espaço do Prestador de Serviços de Saúde")
-        }
+        Element linkPrestador = parser.encontrarLinkPrestador(document)
 
-        if(linkEncontrado) {
-            String urlPrestador = linkEncontrado.absUrl('href')
+        if (linkPrestador) {
+
+            String urlPrestador = linkPrestador.absUrl('href')
 
             Document documentPrestador = HttpBuilder.configure {
                 request.uri = urlPrestador
             }.get()
 
-            println("Página do prestador acessado!")
-            println(document.title())
-        } else {
-            println("Link não encontrado")
-        }
+            Element linkTiss = parser.encontrarLinkTiss(documentPrestador)
 
+            if (linkTiss) {
+
+                String urlTiss = linkTiss.absUrl('href')
+
+                Document documentTiss = HttpBuilder.configure {
+                    request.uri = urlTiss
+                }.get()
+
+                Element linkVersao = parser.encontrarLinkVersao(documentTiss)
+
+                String urlVersao = linkVersao.absUrl('href')
+
+                Document documentVersao = HttpBuilder.configure {
+                    request.uri = urlVersao
+                }.get()
+
+                Element linkComunicacao = parser.encontrarLinkComunicacao(documentVersao)
+
+                String urlComunicacao = linkComunicacao.absUrl('href')
+
+                File diretorio = new File(
+                        "C:/Users/Mysterio/Desktop/Estudo/Estudo Java/TissCrawler/Downloads/Arquivos_padrao_TISS"
+                )
+
+                diretorio.mkdirs()
+
+                File arquivoZip = new File(
+                        diretorio,
+                        'PadroTISSComunicao_202511.zip'
+                )
+
+                if (arquivoZip.exists()) {
+
+                    println("Arquivo já existe. Download não necessário.")
+
+                } else {
+
+                    byte[] arquivo = HttpBuilder.configure {
+                        request.uri = urlComunicacao
+                    }.get(byte[]) {
+                        response.parser('application/zip') { config, resposta ->
+                            resposta.inputStream.bytes
+                        }
+                        }
+
+                    arquivoZip.bytes = arquivo
+
+                    println("Componente de Comunicação baixado!")
+                }
+
+            } else {
+                println("Link da versão não encontrado")
+            }
+
+        } else {
+            println("Link TISS não encontrado")
+        }
     }
 }
